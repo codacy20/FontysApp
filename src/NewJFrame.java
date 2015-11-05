c1641b1716f39af0fa950020e72e8d54673dcad6
Amir Kiumarsi <kmuc72@me.com>
2015-11-05 15:51:32 +0100

    working on upload
    test

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -131,6 +131,8 @@ public class NewJFrame extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        jButton2.setText("Upload");
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
